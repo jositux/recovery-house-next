210d0a6cdf77ef2e44be853c73b17a8daf17fa88
josi <jm.guaimas@gmail.com>
2025-01-30 12:47:49 -0300

    HotFix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -53,7 +53,7 @@ export default function RegistrationPage() {
         email: registrationData.email,
         password: registrationData.password,
         initialRole: registrationData.initialRole,
-        verification_url: "https://recovery-care.vercel.app/"
+        verification_url: "https://recovery-care.vercel.app/user/verify"
       };
       const response = await registerService.register(registerData);
       if (response.challenge) {
