032fdfc43720796aec4efa9b6a950b67853c2021
josi <jm.guaimas@gmail.com>
2025-04-08 22:44:06 -0300

    HotFix

--- server-actions/stripeSession.tsx
+++ server-actions/stripeSession.tsx
@@ -20,7 +20,7 @@ export const postStripeSession = async ({
   unit_amount,
 }: NewSessionOptions) => {
   const returnUrl =
-    "http://recoverycaresolutions.com/checkout-return?session_id={CHECKOUT_SESSION_ID}";
+    "https://recoverycaresolutions.com/checkout-return?session_id={CHECKOUT_SESSION_ID}";
 
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
